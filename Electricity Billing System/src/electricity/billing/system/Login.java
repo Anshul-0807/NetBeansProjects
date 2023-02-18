baa84981d1a32262567e48bc61b17b375bf072da
Anshul patle <patleanshul920@gmail.com>
2023-02-19 00:29:16 +0530

    Update Login.java

--- Electricity Billing System/src/electricity/billing/system/Login.java
+++ Electricity Billing System/src/electricity/billing/system/Login.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class Login extends JFrame implements ActionListener{
+    JButton login, cancel, signup;
     
     Login(){
         super("Login page");
@@ -39,20 +40,23 @@ public class Login extends JFrame implements ActionListener{
         
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/login.png"));
         Image i2 = i1.getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT);
-        JButton login = new JButton("Login", new ImageIcon(i2));
+        login = new JButton("Login", new ImageIcon(i2));
         login.setBounds(330, 160, 100, 20);
+        login.addActionListener(this);
         add(login);
         
         ImageIcon i3 = new ImageIcon(ClassLoader.getSystemResource("icon/cancel.jpg"));
         Image i4 = i3.getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT);
-        JButton cancel = new JButton("Cancel", new ImageIcon(i4));
+        cancel = new JButton("Cancel", new ImageIcon(i4));
         cancel.setBounds(450, 160, 100, 20);
+        cancel.addActionListener(this);
         add(cancel);
         
         ImageIcon i5 = new ImageIcon(ClassLoader.getSystemResource("icon/signup.png"));
         Image i6 = i5.getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT);
-        JButton signup = new JButton("Signup", new ImageIcon(i6) );
+        signup = new JButton("Signup", new ImageIcon(i6) );
         signup.setBounds(380, 200, 100, 20);
+        signup.addActionListener(this);
         add(signup); 
         
         ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icon/second.jpg"));
@@ -67,6 +71,18 @@ public class Login extends JFrame implements ActionListener{
         setVisible(true);
         
     } 
+    
+    public void actionPerformed(ActionEvent ae){
+        if(ae.getSource() == login){
+            
+        }else if (ae.getSource() == cancel){
+            setVisible(false);
+        }else if (ae.getSource() == signup){
+            setVisible(false);
+            
+            new Signup();
+        }
+    }
     
    public static void main(String[] args){
    new Login();
