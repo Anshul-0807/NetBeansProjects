ac51d9ec2fc0c2d1a5a3fbcf52037e0e9aff669c
Anshul patle <patleanshul920@gmail.com>
2023-02-22 19:22:56 +0530

    Create Project.java

---
+++ Electricity Billing System/src/electricity/billing/system/Project.java
@@ -0,0 +1,14 @@
+
+package electricity.billing.system;
+
+public class Project {
+    
+    Project(){
+        
+    }
+    
+    public static void main(String[] args){
+        new Project();
+    }
+            
+}
