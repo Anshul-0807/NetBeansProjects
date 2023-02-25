3275cc94325f3851dccde67a451b7b212391d0ac
Anshul patle <patleanshul920@gmail.com>
2023-02-26 00:09:31 +0530

    Update NewCustomer.java

--- Electricity Billing System/src/electricity/billing/system/NewCustomer.java
+++ Electricity Billing System/src/electricity/billing/system/NewCustomer.java
@@ -3,11 +3,13 @@ package electricity.billing.system;
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
+import java.awt.event.*;
 
-public class NewCustomer extends JFrame {
+
+public class NewCustomer extends JFrame implements ActionListener{
     
-    JTextField tfname, tfaddress, tfstate, tfcity ;
-            
+    JTextField tfname, tfaddress, tfstate, tfcity, tfemail, tfphone ;
+    JButton next, cancel;
     NewCustomer(){
         setSize(700, 500);
         setLocation(400, 200);
@@ -50,20 +52,79 @@ public class NewCustomer extends JFrame {
         tfaddress.setBounds(240, 160, 200, 20);
         p.add(tfaddress);
         
+        JLabel lblcity = new JLabel("City");
+        lblcity.setBounds(100, 200, 100, 20);
+        p.add(lblcity);
+        
+        tfcity = new JTextField();
+        tfcity.setBounds(240, 200, 200, 20);
+        p.add(tfcity);
+        
+        JLabel lblstate = new JLabel("State");
+        lblstate.setBounds(100, 240, 100, 20);
+        p.add(lblstate);
+        
+        tfstate = new JTextField();
+        tfstate.setBounds(240, 240, 200, 20);
+        p.add(tfstate);
         
         
+        JLabel lblemail = new JLabel("Email");
+        lblemail.setBounds(100, 280, 100, 20);
+        p.add(lblemail);
         
+        tfemail = new JTextField();
+        tfemail.setBounds(240, 280, 200, 20);
+        p.add(tfemail);
         
+        JLabel lblphone = new JLabel("Phone Number");
+        lblphone.setBounds(100, 320, 100, 20);
+        p.add(lblphone);
         
+        tfphone = new JTextField();
+        tfphone.setBounds(240, 320, 200, 20);
+        p.add(tfphone);
         
+        next = new JButton("Next");
+        next.setBounds(120, 390, 100, 25);
+        next.setBackground(Color.BLACK);
+        next.setForeground(Color.WHITE);
+        next.addActionListener(this);
+        p.add(next);
         
+        cancel = new JButton("Cancel");
+        cancel.setBounds(250, 390, 100, 25);
+        cancel.setBackground(Color.BLACK);
+        cancel.setForeground(Color.WHITE);
+        cancel.addActionListener(this);
+
+        p.add(cancel);
         
+        setLayout(new BorderLayout());
         
+        add(p, "Center");
         
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/hicon1.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(150, 300, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        add(image, "West"); 
+        
+        getContentPane().setBackground(Color.WHITE);
         
         
         setVisible(true);
+      
+       
     }
+    
+     public void actionPerformed(ActionEvent ae){
+            if (ae.getSource() == next){
+                
+            }else{
+                setVisible(false); 
+            }
+        }
     
    public static void main(String[] args){
     new NewCustomer();
