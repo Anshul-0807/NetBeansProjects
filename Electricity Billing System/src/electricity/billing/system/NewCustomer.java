0c5e2722de1c93abe85ca0b28f418b74dfd77ff4
Anshul patle <patleanshul920@gmail.com>
2023-02-25 22:43:54 +0530

    Create NewCustomer.java

---
+++ Electricity Billing System/src/electricity/billing/system/NewCustomer.java
@@ -0,0 +1,17 @@
+
+package electricity.billing.system;
+import javax.swing.*;
+
+public class NewCustomer extends JFrame {
+ 
+    NewCustomer(){
+        setSize(700, 500);
+        setLocation(400, 200);
+        setVisible(true);
+    }
+    
+   public static void main(String[] args){
+    new NewCustomer();
+   } 
+    
+}
