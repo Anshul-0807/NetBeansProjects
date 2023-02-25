4646cf88ea0f4e252043abffd89dc7e6769a8557
Anshul patle <patleanshul920@gmail.com>
2023-02-25 22:52:16 +0530

    Update NewCustomer.java

--- Electricity Billing System/src/electricity/billing/system/NewCustomer.java
+++ Electricity Billing System/src/electricity/billing/system/NewCustomer.java
@@ -1,12 +1,24 @@
 
 package electricity.billing.system;
 import javax.swing.*;
+import java.awt.*;
 
 public class NewCustomer extends JFrame {
  
     NewCustomer(){
         setSize(700, 500);
         setLocation(400, 200);
+        
+        JPanel p = new JPanel();
+        p.setLayout(null);
+        p.setBackground(new Color(173, 216, 230));
+        add(p);
+        
+        JLabel heading = new JLabel("New Customer");
+        heading.setBounds(180, 10, 200, 25);
+        heading.setFont(new Font("Tahoma", Font.PLAIN, 24 ));
+        p.add(heading);
+        
         setVisible(true);
     }
     
