b515a95e73d408960a24a784fccb11a8b98510e2
Anshul patle <patleanshul920@gmail.com>
2023-02-26 23:05:55 +0530

    Update Conn.java

--- Electricity Billing System/src/electricity/billing/system/Conn.java
+++ Electricity Billing System/src/electricity/billing/system/Conn.java
@@ -10,7 +10,7 @@ public class Conn {
         try{
     c = DriverManager.getConnection("jdbc:mysql:///ebs", "root", "Anshul_123");
     
-//        c = DriverManager.getConnection("jdbc:mysql://localhost:3306/ebs", "root", "Anshul_123");
+        //  c = DriverManager.getConnection("jdbc:mysql://localhost:3306/ebs", "root", "Anshul_123");
 
             s = c.createStatement();
     }catch(Exception e){
